c9361ccb626ff284bc4443d4d3e2996aa2a1801f
jaweria132 <jaweriaalam32@gmail.com>
2021-06-20 11:27:29 +0500

    work done

---
+++ app.js
@@ -0,0 +1,92 @@
+// Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+// var board = [[],[]];
+// 3. Write a program to print numeric counting from 1 to 10.
+// for (var input = 1; input <= 10; input++) {
+//     console.log(input);
+//    }
+
+//    Write a program to print multiplication table of any 
+//    number using for loop. Table number & length should be 
+//    taken as an input from user.
+
+
+// program to generate a multiplication table
+
+// take input from the user
+// var number = prompt('Enter a number to show its multiplication table: ');
+
+//creating a multiplication table
+// for(var i = 1; i <= 10; i++) {
+
+//     // multiply i with number
+//    var result = i * number;
+
+//     // display the result
+//     alert(`${number} * ${i} = ${result}`);
+// }
+
+// // 5. Write a program to print items of the following array 
+// // using for loop:
+// // fruits = [“apple”, “banana”, “mango”, “orange”, 
+// // “strawberry”]
+
+// var fruits = new Array("Apple", "Orange", "Mango", "Banana", "strawberry");
+
+// var new_array = fruits.length;	//no. of elements of the array
+
+// //print elements of the array
+// for (var i = 0; i < new_array; i++) {
+// 	console.log(fruits[i]);
+// }
+
+// var fruits = new Array(5);
+// fruits[0] = "Apple";
+// fruits[1] = "Orage";
+// fruits[2] = "Mango";
+// fruits[3] = "Banana";
+// fruits[4] = "strawberry";
+// console.log(fruits)
+
+// Generate the following series in your browser. See 
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 8. Write a program to identify the largest number in the
+// given array.
+// A = [24, 53, 78, 91, 12].
+// number = [24 , 53 , 78 , 91 , 12];
+// let largest = number[3];
+// document.write("array item is" + largest );
+// document.write("<br>")
+
+
+
+
+
+
+// // Write a program to identify the smallest number in the
+// // given array.
+// // A = [24, 53, 78, 91, 12]
+// number = [24 , 53 , 78 , 91 , 12];
+// let smallest = number[4];
+// document.write("array item is" + smallest );
